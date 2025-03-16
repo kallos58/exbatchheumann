ad18a1bd903c7688df914b746e02e46517592f53
Bernd Maass <bernd.maass_ext@uid.com>
2025-03-16 10:02:02 +0100

    Password

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,15 +3,21 @@ import { Router } from '@angular/router';
 import { MessageService } from '../services/messageService';
 import { DataService } from '../services/dataService';
 import { MaterialModule } from '../material.module';
+import {FormsModule} from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [MaterialModule],
+  imports: [MaterialModule, FormsModule, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
   entries = 0;
+  isPassed = false;
+  invalid = false;
+  username = "bm";
+  password = "";
   constructor( 
     private router: Router,
     private messageService: MessageService,
@@ -22,9 +28,16 @@ export class HomeComponent {
 
   cardClick(i: number) {
     this.messageService.changeIndex("0");
-    debugger;
     if ( i === 8 ) this.router.navigate(['/batchrelease']);
     if ( i === 9 ) this.router.navigate(['/settings']);
+  }
+
+  checkPw() {
+    if (this.username != "bm" || this.password != "oesl") { 
+      this.invalid = true;
+    } else {
+      this.isPassed = true;
+    }
   }
 
   exportToXls() {
